85145b77497a098f1df3b53824cbac24639b97d0
Christopher <91289400+cschultz11@users.noreply.github.com>
2025-04-28 14:37:10 -0400

    Delete backend directory

--- backend/app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import CourseView
-
-urlpatterns = [
-    path("courses/<int:id>/", CourseView.as_view(), name="course-id"),
-    path("courses/", CourseView.as_view(), name="courses"),
-]
